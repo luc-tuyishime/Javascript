a8b817ac359483249afa5fcab791a0bba439de62
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2019-12-24 15:57:57 +0200

    string interpellation and concatenation

---
+++ playground/type.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+const name = 'jean luc';
+const middle = "pierre";
+const last = `yoyo`;
+
+
+const age = 100;
+const Newname = 'tuyishime';
+
+const bombom = 26;
+const kids = 3;
+
+const EachkidGet = Math.floor(bombom / kids);
+console.log(`Each kids gets ${EachkidGet} bomboms`);
+
+const dadGets = bombom % kids;
+console.log(`Dad gets ${dadGets} bomboms`);
