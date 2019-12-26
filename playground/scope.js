97aba00843ba778d859b4b8e37969a32820fb13b
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2019-12-26 16:48:02 +0200

    scope in javascript

---
+++ playground/scope.js
@@ -0,0 +1,27 @@
+const age = 100;
+const dog = 'snickers';
+
+function go() {
+        const hair = 'black';
+        console.log(age);
+}
+
+console.log(age);
+console.log(hair);
+
+function logDog() {
+        console.log(dog);
+}
+
+function newDog() {
+        const dogui = 'luis';
+        logDog('voilaa');
+}
+
+function yell(name) {
+        console.log(name.toUpperCase());
+}
+
+function sayHi(name) {
+        yell(name);
+}
